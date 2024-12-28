ff33b5b1a81d5defd116cfe9118e660cc7210fd5
Viroun KATTYGNARATH <virounk@gmail.com>
2024-12-28 10:03:59 +0100

    error boundaries

---
+++ src/components/back-button.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { ButtonHTMLAttributes } from "react";
+
+type BackButtonProps = {
+  title: string;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | null
+    | undefined;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const BackButton = ({
+  title,
+  className,
+  variant,
+  ...props
+}: BackButtonProps) => {
+  const router = useRouter();
+  return (
+    <Button
+      title={title}
+      className={className}
+      variant={variant}
+      {...props}
+      onClick={() => router.back()}
+    >
+      {title}
+    </Button>
+  );
+};
+
+export default BackButton;
