6a9ebe40b466b9bc5c5944f09281da80f0a948c8
Viroun KATTYGNARATH <virounk@gmail.com>
2025-01-16 11:48:06 +0100

    fruit quantites schema

--- src/zod-schemas/fruitsQuantites.ts
+++ src/zod-schemas/fruitsQuantites.ts
@@ -3,7 +3,7 @@ import { createSelectSchema } from "drizzle-zod";
 
 export const selectFruitsQuantitesSchema = createSelectSchema(fruitsQuantites, {
   effectif: (schema) => schema.min(1, "L'effectif est obligatoire"),
-  kilosParSemaine: (schema) =>
+  kgParSemaine: (schema) =>
     schema.min(1, "Le nombre de kilos par semaine est obligatoires"),
 });
 
