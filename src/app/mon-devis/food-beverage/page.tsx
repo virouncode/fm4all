d1d56f2891baff940a59db49e7714199585dde62
Viroun KATTYGNARATH <virounk@gmail.com>
2025-01-20 02:37:43 +0100

    finished snacks fruits and updated context to have maximum infos for devis final

--- src/app/mon-devis/food-beverage/page.tsx
+++ src/app/mon-devis/food-beverage/page.tsx
@@ -1,34 +1,15 @@
-import Link from "next/link";
 import { Suspense } from "react";
 import ServicesLoader from "../mes-locaux/ServicesLoader";
 import FoodBeverage from "./FoodBeverage";
 
-const page = async ({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | undefined }>;
-}) => {
-  const { cafeFournisseurId } = await searchParams;
-
-  if (cafeFournisseurId && isNaN(parseInt(cafeFournisseurId))) {
-    return (
-      <section className="flex flex-col gap-6">
-        L&apos;identifiant du fournisseur de café n&apos;est pas valide.
-        <Link href="/mon-devis/mes-locaux" className="underline">
-          Veuillez réessayer
-        </Link>
-        .
-      </section>
-    );
-  }
-
+const page = () => {
   return (
     <>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl md:text-4xl">Food & Beverage</h1>
       </div>
       <Suspense fallback={<ServicesLoader />}>
-        <FoodBeverage cafeFournisseurId={cafeFournisseurId} />
+        <FoodBeverage />
       </Suspense>
     </>
   );
