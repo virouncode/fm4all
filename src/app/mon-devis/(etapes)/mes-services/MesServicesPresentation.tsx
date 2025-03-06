62d67e873f952c333914cd8f21efe2c5af882298
Viroun KATTYGNARATH <virounk@gmail.com>
2025-03-06 09:03:22 +0100

    carte moins haute

--- src/app/mon-devis/(etapes)/mes-services/MesServicesPresentation.tsx
+++ src/app/mon-devis/(etapes)/mes-services/MesServicesPresentation.tsx
@@ -160,7 +160,7 @@ const MesServicesPresentation = () => {
               <CarouselItem>
                 <div
                   className={
-                    "bg-fm4allessential flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-48"
+                    "bg-fm4allessential flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-72"
                   }
                 >
                   <p className="text-2xl text-center font-bold">
@@ -179,7 +179,7 @@ const MesServicesPresentation = () => {
               <CarouselItem>
                 <div
                   className={
-                    "bg-fm4allcomfort flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-48"
+                    "bg-fm4allcomfort flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-72"
                   }
                 >
                   <p className="text-2xl text-center font-bold">
@@ -198,7 +198,7 @@ const MesServicesPresentation = () => {
               <CarouselItem>
                 <div
                   className={
-                    "bg-fm4allexcellence flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-48"
+                    "bg-fm4allexcellence flex flex-col items-center justify-center border border-slate-200 rounded-xl p-6 text-white h-72"
                   }
                 >
                   <p className="text-2xl text-center font-bold">
