9957488e4ca6372138ed554f713dd01692ee3441
Viroun KATTYGNARATH <virounk@gmail.com>
2025-03-06 16:26:11 +0100

    repasse

--- src/app/mon-devis/(etapes)/mes-services/(nettoyage)/NettoyageMobileOptionsRepasseCard.tsx
+++ src/app/mon-devis/(etapes)/mes-services/(nettoyage)/NettoyageMobileOptionsRepasseCard.tsx
@@ -99,7 +99,7 @@ const NettoyageMobileOptionsRepasseCard = ({
       <p className="font-bold">Repasse Sanitaire</p>
       <div
         className={`bg-${color} flex flex-col h-72 border border-slate-200 rounded-xl p-4 text-white  ${
-          nettoyage.infos.repasseSelected
+          nettoyage.infos.repasseSelected && repasseProposition
             ? "ring-4 ring-inset ring-fm4alldestructive"
             : ""
         }`}
