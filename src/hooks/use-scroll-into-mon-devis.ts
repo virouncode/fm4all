73ebac993e8d081f12e43ffb8d8df43630a0cd40
Viroun KATTYGNARATH <virounk@gmail.com>
2025-03-13 01:16:53 +0100

    scroll into devis

--- src/hooks/use-scroll-into-mon-devis.ts
+++ src/hooks/use-scroll-into-mon-devis.ts
@@ -1,14 +1,17 @@
 import { MonDevisContext } from "@/context/MonDevisProvider";
 import { useContext, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function useScrollIntoMonDevis() {
   const { monDevis } = useContext(MonDevisContext);
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1024px)" });
   useEffect(() => {
+    if (!isTabletOrMobile || monDevis.currentMonDevisId !== 2) return;
     const currentMonDevis = document.getElementById(
       monDevis.currentMonDevisId.toString()
     );
     if (currentMonDevis) {
       currentMonDevis.scrollIntoView({ behavior: "smooth", block: "end" });
     }
-  }, [monDevis.currentMonDevisId]);
+  }, [monDevis.currentMonDevisId, isTabletOrMobile]);
 }
