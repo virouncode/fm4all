7dc6434239c85698174e9cac5cf890821e11f510
Viroun KATTYGNARATH <virounk@gmail.com>
2025-03-17 16:55:47 +0100

    started blog

---
+++ src/app/posts/[parent-slug]/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import Article from "@/components/Article";
+import { client } from "@/sanity/client";
+import { ARTICLE_TRANSLATIONS_QUERY } from "@/sanity/lib/queries";
+import { ArticleType } from "@/sanity/sanity.types";
+
+import { type SanityDocument } from "next-sanity";
+import { Suspense } from "react";
+
+const options = { next: { revalidate: 30 } };
+
+export default async function page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const results = await client.fetch<SanityDocument>(
+    ARTICLE_TRANSLATIONS_QUERY,
+    await params,
+    options
+  );
+  console.log("params", await params);
+
+  console.log("results", results);
+
+  const articleTranslations: ArticleType[] = results[0]._translations;
+  return (
+    <Suspense>
+      <Article translations={articleTranslations} />
+    </Suspense>
+  );
+}
