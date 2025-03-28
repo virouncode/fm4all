d69d633b6c048ab4385ed7207cfb9fdfdcbc7fb5
Viroun KATTYGNARATH <virounk@gmail.com>
2025-03-28 19:01:53 +0100

    ssr services pour maj

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,6 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: "",
-        pathname: "/**",
-        search: "",
       },
     ],
   },
