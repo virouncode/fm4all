e9fc02af9fddfcd901caf4161cdc88b194ba8cfc
Viroun KATTYGNARATH <virounk@gmail.com>
2025-04-29 11:37:58 +0200

    SEO

--- src/lib/metadata-helpers.ts
+++ src/lib/metadata-helpers.ts
@@ -84,8 +84,8 @@ export const routes: Record<RouteKey, Record<"fr" | "en", string>> = {
     en: "/become-a-provider",
   },
   services: {
-    fr: "/nos-services",
-    en: "/our-services",
+    fr: "/services",
+    en: "/services",
   },
   servicePresentation: {
     fr: "/services/[slug]",
@@ -144,73 +144,6 @@ export const routes: Record<RouteKey, Record<"fr" | "en", string>> = {
     en: "/sectors/[slug]",
   },
 };
-
-export const routeMapping: Record<"fr" | "en", Record<string, string>> = {
-  en: {
-    "/": "/",
-    "/conditions-generales-d-utilisation": "/terms-and-conditions-of-use",
-    "/conditions-generales-de-vente": "/sales-terms-and-conditions",
-    "/zone-non-couverte": "/area-not-covered",
-    "/politique-de-confidentialite": "/privacy-policy",
-    "/contactez-nous": "/contact-us",
-    "/politique-de-cookies": "/cookie-policy",
-    "/nos-engagements": "/our-commitments",
-    "/foire-aux-questions": "/frequently-asked-questions",
-    "/nos-3-gammes": "/our-3-tiers",
-    "/mentions-legales": "/legal-notices",
-    "/nos-partenaires": "/our-partners",
-    "/devenir-prestataire": "/become-a-provider",
-    "/nos-services": "/our-services",
-    "/mon-devis/mes-locaux": "/my-quote/my-premises",
-    "/mon-devis/mes-services": "/my-quote/my-services",
-    "/mon-devis/food-beverage": "/my-quote/food-beverage",
-    "/mon-devis/pilotage-prestations": "/my-quote/service-management",
-    "/mon-devis/sauvegarder-ma-progression": "/my-quote/save-my-progress",
-    "/mon-devis/personnaliser-mon-devis": "/my-quote/customize-my-quote",
-    "/mon-devis/afficher-mon-devis": "/my-quote/view-my-quote",
-  },
-  fr: {
-    "/": "/",
-    "/terms-and-conditions-of-use": "/conditions-generales-d-utilisation",
-    "/sales-terms-and-conditions": "/conditions-generales-de-vente",
-    "/area-not-covered": "/zone-non-couverte",
-    "/privacy-policy": "/politique-de-confidentialite",
-    "/contact-us": "/contactez-nous",
-    "/cookie-policy": "/politique-de-cookies",
-    "/our-commitments": "/nos-engagements",
-    "/frequently-asked-questions": "/foire-aux-questions",
-    "/our-3-tiers": "/nos-3-gammes",
-    "/legal-notices": "/mentions-legales",
-    "/our-partners": "/nos-partenaires",
-    "/become-a-provider": "/devenir-prestataire",
-    "/our-services": "/nos-services",
-    "/my-quote/my-premises": "/mon-devis/mes-locaux",
-    "/my-quote/my-services": "/mon-devis/mes-services",
-    "/my-quote/food-beverage": "/mon-devis/food-beverage",
-    "/my-quote/service-management": "/mon-devis/pilotage-prestations",
-    "/my-quote/save-my-progress": "/mon-devis/sauvegarder-ma-progression",
-    "/my-quote/customize-my-quote": "/mon-devis/personnaliser-mon-devis",
-    "/my-quote/view-my-quote": "/mon-devis/afficher-mon-devis",
-  },
-};
-
-export const isValidPathForLocale = (
-  pathWithoutLocale: string,
-  locale: "fr" | "en"
-): boolean => {
-  const validPaths = Object.values(routes).map((route) => route[locale]);
-  // console.log("validPaths", validPaths);
-  if (validPaths.includes(pathWithoutLocale)) return true;
-  return false;
-};
-
-export function getEquivalentPath(
-  pathWithoutLocale: string,
-  locale: "fr" | "en"
-): string | null {
-  if (isValidPathForLocale(pathWithoutLocale, locale)) return pathWithoutLocale;
-  return routeMapping[locale][pathWithoutLocale] || "/404";
-}
 
 export function generateAlternates(
   routeKey: RouteKey,
