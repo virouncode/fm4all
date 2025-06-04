759785a45be0f60d502485e184f3c3be87b2e8a2
Viroun KATTYGNARATH <virounk@gmail.com>
2025-06-04 16:57:32 +0200

    alternates defaut

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "fr"],
   defaultLocale: "fr",
+  alternateLinks: false,
   pathnames: {
     "/": {
       fr: "/",
