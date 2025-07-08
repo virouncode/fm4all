4608650ff1ee5d1a87f0fe2474c97ead3cb3d81f
Viroun KATTYGNARATH <virounk@gmail.com>
2025-07-08 18:02:57 +0200

    nettoyage ville pages middleware redirects

--- src/redirects/handleRedirects.ts
+++ src/redirects/handleRedirects.ts
@@ -7,7 +7,12 @@ import {
   getArticlesSubSlugEn,
   getArticlesSubSlugFr,
 } from "./articlesSlugMappings";
-import { isEnglishSlug, isEnglishSubSlug, isFrenchSlug } from "./isEnglishSlug";
+import {
+  isEnglishSlug,
+  isEnglishSubSlug,
+  isFrenchSlug,
+  isFrenchSubSlug,
+} from "./isEnglishSlug";
 import {
   getSecteurSlugEn,
   getSecteurSlugFr,
@@ -16,7 +21,10 @@ import {
 import {
   getServicesSlugEn,
   getServicesSlugFr,
+  getServicesSubSlugEn,
+  getServicesSubSlugFr,
   servicesSlugMappingsFrToEn,
+  servicesSubSlugMappingsFrToEn,
 } from "./servicesSlugMappings";
 
 export const handleArticleRedirects = (
@@ -40,7 +48,7 @@ export const handleArticleRedirects = (
 
   const isPossibleSubSlug = subSlug
     ? isEnglishSubSlug(subSlug, articlesSubSlugMappingsFrToEn) ||
-      isFrenchSlug(subSlug, articlesSubSlugMappingsFrToEn)
+      isFrenchSubSlug(subSlug, articlesSubSlugMappingsFrToEn)
     : true;
 
   if (!isPossibleSlug && !isPossibleSubSlug) return null;
@@ -109,11 +117,19 @@ export const handleServiceRedirects = (
 
   const slug = pathSegments[1];
   const basePath = "/services/";
+  const subSlug = pathSegments.length >= 3 ? pathSegments[2] : null;
 
   const isPossibleSlug =
     isEnglishSlug(slug, servicesSlugMappingsFrToEn) ||
     isFrenchSlug(slug, servicesSlugMappingsFrToEn);
   if (!isPossibleSlug) return null;
+
+  const isPossibleSubSlug = subSlug
+    ? isEnglishSubSlug(subSlug, servicesSubSlugMappingsFrToEn) ||
+      isFrenchSubSlug(subSlug, servicesSubSlugMappingsFrToEn)
+    : true;
+
+  if (!isPossibleSlug && !isPossibleSubSlug) return null;
 
   const isCorrectSlug =
     locale === "fr"
@@ -123,8 +139,43 @@ export const handleServiceRedirects = (
     const correctSlug =
       locale === "fr" ? getServicesSlugFr(slug) : getServicesSlugEn(slug);
     const newPath = `/${locale}${basePath}${correctSlug}`;
+    if (subSlug) {
+      const isCorrectSubSlug =
+        locale === "fr"
+          ? !isEnglishSubSlug(subSlug, servicesSubSlugMappingsFrToEn)
+          : isEnglishSubSlug(subSlug, servicesSubSlugMappingsFrToEn);
+      const correctSubSlug = isCorrectSubSlug
+        ? subSlug
+        : locale === "fr"
+          ? getServicesSubSlugFr(subSlug)
+          : getServicesSubSlugEn(subSlug);
+
+      return NextResponse.redirect(
+        new URL(`${newPath}/${correctSubSlug}`, req.url),
+        301
+      );
+    }
+
     return NextResponse.redirect(new URL(newPath, req.url), 301);
   }
+  // Si le slug principal est correct mais qu'on a un sous-slug incorrect
+  if (isCorrectSlug && subSlug) {
+    const isCorrectSubSlug =
+      locale === "fr"
+        ? !isEnglishSubSlug(subSlug, servicesSubSlugMappingsFrToEn)
+        : isEnglishSubSlug(subSlug, servicesSubSlugMappingsFrToEn);
+    if (!isCorrectSubSlug) {
+      const correctSubSlug =
+        locale === "fr"
+          ? getServicesSubSlugFr(subSlug)
+          : getServicesSubSlugEn(subSlug);
+      return NextResponse.redirect(
+        new URL(`/${locale}${basePath}${slug}/${correctSubSlug}`, req.url),
+        301
+      );
+    }
+  }
+  // Tout est correct, on ne fait pas de redirection
   return null;
 };
 
