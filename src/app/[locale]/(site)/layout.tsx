5440ff7f6a730fb5041e5be2650d2bff514ac305
Viroun KATTYGNARATH <virounk@gmail.com>
2025-07-09 14:55:14 +0200

    correction traduction legende devis pilotage fm4all

--- src/app/[locale]/(site)/layout.tsx
+++ src/app/[locale]/(site)/layout.tsx
@@ -108,7 +108,7 @@ export default async function LocalizedLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body
-        className={`${didact.className} antialiased scroll-smooth tracking-[-0.02em]`}
+        className={`${didact.className} antialiased scroll-smooth tracking-[-0.015em]`}
       >
         <GoogleAnalytics
           GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}
