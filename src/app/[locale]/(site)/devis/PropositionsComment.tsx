f0085c8d527013df12680c12fb372e93b67fe494
Viroun KATTYGNARATH <virounk@gmail.com>
2025-09-03 02:14:33 +0200

    suppressions des fichiers en trop

--- src/app/[locale]/(site)/devis/PropositionsComment.tsx
+++
@@ -1,9 +0,0 @@
-type PropositionsCommentProps = {
-  text: string;
-};
-
-const PropositionsComment = ({ text }: PropositionsCommentProps) => {
-  return <p className="px-1 text-end text-sm italic">{text}</p>;
-};
-
-export default PropositionsComment;
