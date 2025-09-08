48884108714bc93d7a05df9bd10a5b6b73d4ca93
Viroun KATTYGNARATH <virounk@gmail.com>
2025-09-08 11:29:56 +0200

    ajouté priority à l'image hero

--- src/components/backgrounds/BackgroundServer.tsx
+++ src/components/backgrounds/BackgroundServer.tsx
@@ -14,6 +14,7 @@ const BackgroundServer = async () => {
         quality={75}
         fill
         sizes="100vw"
+        priority
       />
       <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-black/90"></div>
     </div>
