1b07514c549afe078c3c9b76d070980d41a597d7
Viroun KATTYGNARATH <virounk@gmail.com>
2025-09-11 22:26:18 +0200

    locale dans le devis

--- src/app/[locale]/(main)/(application)/layout.tsx
+++ src/app/[locale]/(main)/(application)/layout.tsx
@@ -1,22 +1,9 @@
 import ContextWrapper from "@/context/ContextWrapper";
-import { routing } from "@/i18n/routing";
-import { hasLocale } from "next-intl";
-import { setRequestLocale } from "next-intl/server";
-import { notFound } from "next/navigation";
 
-export default async function ApplicationLayout({
+export default function ApplicationLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
 }>) {
-  const { locale } = await params;
-
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
-  setRequestLocale(locale);
-
   return <ContextWrapper>{children}</ContextWrapper>;
 }
