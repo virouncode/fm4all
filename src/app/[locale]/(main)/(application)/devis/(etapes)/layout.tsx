2cd3f67a715ff7f65964ab503a5426a5f685641a
Viroun KATTYGNARATH <virounk@gmail.com>
2025-09-29 16:05:45 +0200

    devis en maintenance

--- src/app/[locale]/(main)/(application)/devis/(etapes)/layout.tsx
+++ src/app/[locale]/(main)/(application)/devis/(etapes)/layout.tsx
@@ -1,20 +1,21 @@
-import CacheInvalidationListener from "@/components/cache/CacheInvalidationListener";
-import DevisBreadcrumb from "../DevisBreadcrumb";
-import Total from "../Total";
-
 export default function MonDevisLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main className="mx-auto flex max-w-7xl flex-col gap-4 px-6 pb-10 pt-4 md:px-20 lg:h-[calc(100vh-4rem)]">
-      <DevisBreadcrumb />
+    <main className="mx-auto flex max-w-7xl flex-col gap-4 px-6 pt-4 pb-10 md:px-20 lg:h-[calc(100vh-4rem)]">
+      {/* <DevisBreadcrumb />
       <div className="lg:relative">
         <Total />
       </div>
       {children}
-      <CacheInvalidationListener />
+      <CacheInvalidationListener /> */}
+      <div>
+        Cette partie du site est en maintenance. Veuillez revenir plus tard.
+        <br />
+        This part of the website is under maintenance, please come back later.
+      </div>
     </main>
   );
 }
